078ac33e6faa16b9158cb60ba90ff24dd3d613fe
kaamibb <120927463+kaamibb@users.noreply.github.com>
2022-12-18 19:56:14 +0700

    Update and rename load bar.cpp to Final Project_Kelompok 3.cpp

--- Final Project_Kelompok 3.cpp
+++ Final Project_Kelompok 3.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <conio.h>
-#include <unistd.h>
 #include <windows.h>
 #include <iostream>
 
@@ -20,27 +17,26 @@ void loadbar()
 {
 	int bar1=177, bar2=219;
 	cout<<"Loading...\n";
-  cout<<"\n\t\t\t\t\t\t";
-  for(int i=0; i<25; i++)
+  	cout<<"\n\t\t\t\t\t\t";
+  	for(int i=0; i<25; i++)
 	  cout<< (char)bar1;
 	  
 	cout<<"\r";
 	cout<<"\t\t\t\t\t\t";
   	for(int i=0; i<25; i++)
-  	{
-  		
+  	{	
   		cout<< (char)bar2;
 		Sleep(150);	  
-		  }
+	}
 		  
 	cout<<"\n\t\t\t\t\t\t";
 	system("pause");
 }
     
 int main(){
-  system("color 74");
-  system("cls");
-  gotoxy(48,11);
+  	system("color 74");
+  	system("cls");
+  	gotoxy(48,11);
 	loadbar();
 	system("cls");
 }
